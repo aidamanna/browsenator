d20963cadf2ef1c3603065a8bd0b8ee1ba87a536
Aida Manna <aida@typeform.com>
2018-05-04 13:55:05 +0200

    Check in tests that the opened browser type is correct

--- spec/local/chrome_spec.rb
+++ spec/local/chrome_spec.rb
@@ -2,7 +2,10 @@ describe Chrome do
   describe '#open' do
     it 'opens a chrome browser' do
       browser = Chrome.new.open
+      browser_type = browser.driver.browser
+
       expect(browser).to be_a(Watir::Browser)
+      expect(browser_type).to eql(:chrome)
     end
   end
 end
--- spec/local/headless_chrome_spec.rb
+++ spec/local/headless_chrome_spec.rb
@@ -2,7 +2,10 @@ describe HeadlessChrome do
   describe '#open' do
     it 'starts a headless chrome browser' do
       browser = HeadlessChrome.new.open
+      browser_type = browser.driver.browser
+
       expect(browser).to be_a(Watir::Browser)
+      expect(browser_type).to eql(:chrome)
     end
   end
 end
