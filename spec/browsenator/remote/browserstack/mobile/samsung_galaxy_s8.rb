0de68b9a418cfdeece248ef9c0a44c31439eea04
Aida Manna <aida@typeform.com>
2018-07-04 18:47:48 +0200

    Add SamsungGalaxyS8 remote mobile browserstack platform

---
+++ spec/browsenator/remote/browserstack/mobile/samsung_galaxy_s8.rb
@@ -0,0 +1,18 @@
+describe Browsenator::Remote::Browserstack::Mobile::SamsungGalaxyS8 do
+  describe '#open' do
+    after(:each) do
+      @browser.quit
+    end
+
+    it 'starts Chrome in a linux device' do
+      @browser = described_class.new(project: 'Test').open
+      browser_type = @browser.driver.capabilities.browser_name
+      operating_system = @browser.driver.capabilities.platform
+
+      expect(@browser).to be_a(Watir::Browser)
+      expect(@browser.driver).to be_a(Selenium::WebDriver::Remote::Driver)
+      expect(browser_type).to eql('chrome')
+      expect(operating_system).to match('linux')
+    end
+  end
+end
